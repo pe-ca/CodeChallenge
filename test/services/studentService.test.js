3e7b8797b74cc4bba2b892fdedc71159054b1f97
pe-ca <mr.gloser1@gmail.com>
2022-05-09 19:03:20 -0500

    agregados casos de prueba para metodo studentEmailWithCertification

--- test/services/studentService.test.js
+++ test/services/studentService.test.js
@@ -3,13 +3,21 @@ const reader = require("./../../lib/utils/reader")
 
 describe("suite de pruebas para studentService", () =>{
     test("Comprobacion de existencia de metodos", ()=>{
-        const result = typeof ss.studentList
-        expect(result).toBe("function")
+        const students = typeof ss.studentList
+        const email = typeof ss.studentEmailWithCertification
+        expect(students).toBe("function")
+        expect(email).toBe("function")
     })
 
     test("regresa un objeto con un listado de todos los estudiantes", () =>{
         const studentList = reader.readJsonFile("./test/testStudents.json")
         const result = ss.studentList("./test/testStudents.json")
         expect(result).toEqual(studentList)
+    })
+
+    test("regresa una listade correos de los estudiantes con certificacion: true", () =>{
+        const emailList = ["Todd@visualpartnership.xyz","Sexton@visualpartnership.xyz"]
+        const result = ss.studentEmailWithCertification("./test/testStudents.json")
+        expect(result).toEqual(emailList)
     })
 })
