0ab27bedd035915a65b144260d31c76693b9b21c
pe-ca <mr.gloser1@gmail.com>
2022-05-10 11:22:07 -0500

    agregadas pruebas unitarias para studentsWithCreditsOver500

--- test/services/studentService.test.js
+++ test/services/studentService.test.js
@@ -5,8 +5,10 @@ describe("suite de pruebas para studentService", () =>{
     test("Comprobacion de existencia de metodos", ()=>{
         const students = typeof ss.studentList
         const email = typeof ss.studentEmailWithCertification
+        const credits = typeof ss.studentsWithCreditsOver500
         expect(students).toBe("function")
         expect(email).toBe("function")
+        expect(credits).toBe("function")
     })
 
     test("regresa un objeto con un listado de todos los estudiantes", () =>{
@@ -19,5 +21,11 @@ describe("suite de pruebas para studentService", () =>{
         const emailList = ["Todd@visualpartnership.xyz","Sexton@visualpartnership.xyz"]
         const result = ss.studentEmailWithCertification("./test/testStudents.json")
         expect(result).toEqual(emailList)
+    })
+
+    test("regresa una lista de nombres de los estudiantes con mas de 500 creditos", () =>{
+        const credits = ["Warren","Lucinda"]
+        const result = ss.studentsWithCreditsOver500("./test/testStudents.json")
+        expect(result).toEqual(credits)
     })
 })
