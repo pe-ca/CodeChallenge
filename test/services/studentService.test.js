b6f056619c4dc5dd695cf61ba0ccebf7e48a8c7b
pe-ca <mr.gloser1@gmail.com>
2022-05-10 11:57:49 -0500

    correccion de estilos

--- test/services/studentService.test.js
+++ test/services/studentService.test.js
@@ -1,31 +1,31 @@
-const ss = require("../../lib/services/studentService")
-const reader = require("./../../lib/utils/reader")
+const ss = require("../../lib/services/studentService");
+const reader = require("./../../lib/utils/reader");
 
 describe("suite de pruebas para studentService", () =>{
     test("Comprobacion de existencia de metodos", ()=>{
-        const students = typeof ss.studentList
-        const email = typeof ss.studentEmailWithCertification
-        const credits = typeof ss.studentsWithCreditsOver500
-        expect(students).toBe("function")
-        expect(email).toBe("function")
-        expect(credits).toBe("function")
-    })
+        const students = typeof ss.studentList;
+        const email = typeof ss.studentEmailWithCertification;
+        const credits = typeof ss.studentsWithCreditsOver500;
+        expect(students).toBe("function");
+        expect(email).toBe("function");
+        expect(credits).toBe("function");
+    });
 
     test("regresa un objeto con un listado de todos los estudiantes", () =>{
-        const studentList = reader.readJsonFile("./test/testStudents.json")
-        const result = ss.studentList("./test/testStudents.json")
-        expect(result).toEqual(studentList)
-    })
+        const studentList = reader.readJsonFile("./test/testStudents.json");
+        const result = ss.studentList("./test/testStudents.json");
+        expect(result).toEqual(studentList);
+    });
 
     test("regresa una listade correos de los estudiantes con certificacion: true", () =>{
-        const emailList = ["Todd@visualpartnership.xyz","Sexton@visualpartnership.xyz"]
-        const result = ss.studentEmailWithCertification("./test/testStudents.json")
-        expect(result).toEqual(emailList)
-    })
+        const emailList = ["Todd@visualpartnership.xyz","Sexton@visualpartnership.xyz"];
+        const result = ss.studentEmailWithCertification("./test/testStudents.json");
+        expect(result).toEqual(emailList);
+    });
 
     test("regresa una lista de nombres de los estudiantes con mas de 500 creditos", () =>{
-        const credits = ["Warren","Lucinda"]
-        const result = ss.studentsWithCreditsOver500("./test/testStudents.json")
-        expect(result).toEqual(credits)
-    })
-})
+        const credits = ["Warren","Lucinda"];
+        const result = ss.studentsWithCreditsOver500("./test/testStudents.json");
+        expect(result).toEqual(credits);
+    });
+});
--- test/utils/reader.test.js
+++ test/utils/reader.test.js
@@ -1,19 +1,19 @@
-const reader = require("./../../lib/utils/reader")
+const reader = require("./../../lib/utils/reader");
 
 describe("Suite de pruebas para reader.js", () =>{
     test("Comprobacion de existencia de metodo", ()=>{
-        const result = typeof reader.readJsonFile
-        expect(result).toBe("function")
-    })
+        const result = typeof reader.readJsonFile;
+        expect(result).toBe("function");
+    });
 
     test("Regresa un objeto con la informacion leida", ()=>{
-        const result = reader.readJsonFile("./test/testStudents.json")
-        expect(typeof result).toBe("object")
-    })
+        const result = reader.readJsonFile("./test/testStudents.json");
+        expect(typeof result).toBe("object");
+    });
 
     test("Lanza un throw en caso de que no se envie un parametro correcto", ()=>{
         expect(() => {
-            reader.readJsonFile("")
-        }).toThrow()
-    })
-})
+            reader.readJsonFile("");
+        }).toThrow();
+    });
+});
